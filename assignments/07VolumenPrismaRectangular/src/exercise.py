a62a04536c06b7e73983e04c1b36d292f944772c
CitlaliRodsan <88851035+CitlaliRodsan@users.noreply.github.com>
2021-08-29 13:28:20 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,7 +1,21 @@
+def areaRect(base, altura):
+    area=base*altura
+    return area
+
+def volPrism(profundo):
+    base=areaRect(b,h)
+    vol=base*profundo
+    return vol
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    global b
+    global h
+    b=float(input("Dame la base: "))
+    h=float(input("Dame la altura: "))
+    p=float(input("Dame la profundidad: "))
+
+    print("El volumen del prisma es:", volPrism(p))
 
 if __name__=='__main__':
     main()
